e022ed04308ac1424adde6a9ddacf8dc2a5d6266
GiM6114 <73024037+GiM6114@users.noreply.github.com>
2023-08-04 00:07:07 +0200

    start loss etc

--- agent.py
+++ agent.py
@@ -2,6 +2,7 @@ import random
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.distributions as distributions
 import torch
 
 from board import Board
@@ -50,7 +51,8 @@ class RandomPlayer(Player):
 
 class PlayerAC(Player):
     
-    def __init__(self, env, gamma, lr_a, lr_c):
+    def __init__(self, env, gamma, lr_a, lr_c,
+                 coordinate_std):
         
         self.gamma = gamma
         
@@ -62,6 +64,7 @@ class PlayerAC(Player):
         self.actor = NeuralNetwork(
             input_size=self.shared_rep_size, 
             output_size=2+4, 
+            # coordinates to place previous tile + which tile to take
             l=5, 
             n=150)
         self.critic = NeuralNetwork(
@@ -72,10 +75,62 @@ class PlayerAC(Player):
         params = list(self.shared.parameters) + \
                  list(self.actor.parameters) + \
                  list(self.critic.parameters)
-        self.optimizer = torch.optim.Adam(params)
+   
+        self.coordinates_cov = torch.eye(2) * coordinate_std**2
+   
+        self.optimizer_c = torch.optim.AdamW(
+            self.shared.parameters() + self.critic.parameters(),
+            amsgrad=True,
+            lr=0.001)
+        self.criterion_c = nn.SmoothL1Loss()
+        
+        self.optimizer_a = torch.optim.AdamW(
+            self.shared.parameters() + self.actor.parameters(),
+            amsgrad=True,
+            lr=0.0005)
+        self.criterion_a = nn.SmoothL1Loss()
+        
+        self.reset()
+        
+    def reset(self):
+        self.reward = None
+        self.action = None
+        self.value = None
     
     def action(self, state):
+        self.prev_value = self.value
         shared_rep = self.shared(state)
-        self.ac
- 
+        action_output = self.actor(shared_rep)
+        
+        coordinates_output = action_output[:2]
+        coordinates_distribution = distributions.MultivariateNormal(coordinates_output, self.coordinates_cov)
+        coordinates = coordinates_distribution.sample()
+        
+        tile_output_distribution = F.softmax(action_output[2:], dim=0)
+        tile = torch.multinomial(tile_output_distribution, num_sample=1)
+                
+        self.value = self.critic(shared_rep)
+        
+        # Updates
+        if self.prev_value is not None:
+            target = self.reward + self.gamma*self.value
+            
+            loss_c = self.criterion_c(
+                target, 
+                self.value)
+            self.optimizer_c.zero_grad()
+            loss_c.backward
+            self.optimizer_c.step()
+            
+            loss_a = -torch.log() * loss_c
+            self.optimizer_c.zero_grad()
+            loss_c.backward
+            self.optimizer_c.step()
+            
+        return 
+            
+            
+
+    def give_reward(self, reward):
+        self.reward = reward
 
