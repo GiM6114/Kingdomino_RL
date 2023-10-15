pb grdt computation fix in minimal file not entirely satisfactory

agent.py
@@ -4,6 +4,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.distributions as distributions
 import torch
+torch.autograd.set_detect_anomaly(True)
+from collections import namedtuple
+from torchviz import make_dot
+from IPython.display import display
 
 from board import Board
 from setup import N_TILE_TYPES
@@ -31,103 +35,129 @@ class RandomPlayer:
         return (kingdomino.selectTileRandom(),
                 kingdomino.selectTilePositionRandom())
 
+NetworksInfo = namedtuple('NetworksInfo', 
+                          ['in_channels', 
+                           'output_size',
+                           'conv_channels',
+                           'conv_l',
+                           'conv_kernel_size', 
+                           'conv_stride',
+                           'pool_place',
+                           'pool_kernel_size', 
+                           'pool_stride',
+                           'board_rep_size',
+                           'board_fc_n',
+                           'board_fc_l',
+                           'player_rep_size',
+                           'board_prev_tile_fc_l',
+                           'shared_rep_size',
+                           'shared_l',
+                           'shared_n',
+                           'actor_l',
+                           'actor_n',
+                           'critic_l',
+                           'critic_n'])
 
 class PlayerAC:
     
-    def __init__(self, n_players, gamma, lr_a, lr_c,
-                 coordinate_std, _id):
+    def __init__(self, 
+                 batch_size,
+                 n_players, 
+                 gamma, lr_a, lr_c, coordinate_std,
+                 network_info):
         
         self.gamma = gamma
-        
-        self.shared_rep_size = 200
         self.shared = Shared(
             n_players, 
-            board_rep_size=100,
-            shared_rep_size=self.shared_rep_size)
-        self.actor = NeuralNetwork(
-            input_size=self.shared_rep_size, 
-            output_size=2+4, 
-            # coordinates to place previous tile + which tile to take
-            l=5, 
-            n=150)
+            network_info)
+
         self.critic = NeuralNetwork(
-            input_size=self.shared_rep_size, 
+            input_size=network_info.shared_rep_size, 
             output_size=1, 
-            l=5, 
-            n=150)
-        # params = list(self.shared.parameters) + \
-        #          list(self.actor.parameters) + \
-        #          list(self.critic.parameters)
+            l=network_info.critic_l, 
+            n=network_info.critic_n)
+        self.actor = NeuralNetwork(
+            input_size=network_info.shared_rep_size, 
+            output_size=2+n_players, 
+            # coordinates to place previous tile + which tile to take
+            l=network_info.actor_l, 
+            n=network_info.actor_n)
    
-        self.coordinates_cov = torch.eye(2) * coordinate_std**2
-   
+        self.coordinates_cov = torch.eye(2).repeat(batch_size,1,1) * coordinate_std**2
+        
         self.optimizer_c = torch.optim.AdamW(
             list(self.shared.parameters()) + list(self.critic.parameters()),
             amsgrad=True,
-            lr=0.001)
+            lr=lr_c)
         self.criterion_c = nn.SmoothL1Loss()
-        
         self.optimizer_a = torch.optim.AdamW(
             list(self.shared.parameters()) + list(self.actor.parameters()),
             amsgrad=True,
-            lr=0.0005)
-        self.criterion_a = nn.SmoothL1Loss()
+            lr=lr_a)
         
         self.reset()
         
     def reset(self):
-        super().reset()
-        self.reward = None
-        self.tile_choice_distribution = None
-        self.tile_chosen = None
-        self.coordinates_distribution = None
+        self.rewards = None
+        self.tile_choice_distributions = None
+        self.tiles_chosen = None
+        self.coordinates_distributions = None
         self.coordinates_chosen = None
-        self.value = None
+        self.values = None
     
-    def action(self, state):
-        shared_rep = self.shared(self.id, state)
+    def action(self, states):
+        shared_rep = self.shared(states)
+
+        print('Version :', shared_rep._version)
         
         # Here we have the previous state value, action, reward and state is the next state
         # -> Update
-        self.prev_value = self.value
-        self.value = self.critic(shared_rep)
+        self.prev_values = self.values
+        self.values = self.critic(shared_rep).squeeze()
+        graph = make_dot(self.critic(shared_rep), params=dict(self.shared.named_parameters()) | dict(self.critic.named_parameters()))
+        display(graph)
 
-        if self.prev_value is not None:
-            target = self.reward + self.gamma*self.value
-            
+        if self.prev_values is not None:
+            targets = self.rewards + self.gamma*self.values
             # Critic update
             loss_c = self.criterion_c(
-                target,
-                self.prev_value)
+                targets,
+                self.prev_values)
             self.optimizer_c.zero_grad()
-            loss_c.backward()
+            loss_c.backward(retain_graph=True)
             self.optimizer_c.step()
-            
+            print('Version :', shared_rep._version)
+            print('Version :', loss_c._version)
+
             # Actor update
             # product of proba as tile choice and coordinates chosen for previous tiles
             # assumeed independant (they are not)
-            loss_a = (-self.coordinates_distribution.log_prob(self.coordinates_chosen) \
-                     -self.tile_choice_distribution.log_prob(self.tile_chosen)) * loss_c
+            loss_a = torch.mean((-self.coordinates_distributions.log_prob(self.coordinates_chosen) \
+                     -self.tile_choice_distributions.log_prob(self.tile_chosen))) * loss_c
+            print('Version :', shared_rep._version)
+            print('Version :', loss_a._version)
             self.optimizer_a.zero_grad()
             loss_a.backward()
             self.optimizer_a.step()
         
-        action_output = self.actor(shared_rep)
+        action_outputs = self.actor(shared_rep)
+        graph = make_dot(self.actor(shared_rep), params=dict(self.shared.named_parameters()) | dict(self.actor.named_parameters()))
+        display(graph)
         
-        coordinates_output = action_output[:2]
-        self.coordinates_distribution = distributions.MultivariateNormal(coordinates_output, self.coordinates_cov)
-        self.coordinates_chosen = torch.round(self.coordinates_distribution.sample())
+        coordinates_outputs = action_outputs[:,:2]
+        self.coordinates_distributions = distributions.MultivariateNormal(coordinates_outputs, self.coordinates_cov)
+        self.coordinates_chosen = torch.round(self.coordinates_distributions.sample())
         
-        tile_output_distribution = F.softmax(action_output[2:], dim=0)
-        self.tile_choice_distribution = distributions.Multinomial(tile_output_distribution)
-        self.tile_chosen = self.tile_choice_distribution.sample()
+        tile_output_distribution = F.softmax(action_outputs[:,2:], dim=1)
+        self.tile_choice_distributions = distributions.Multinomial(total_count=1, probs=tile_output_distribution)
+        self.tile_chosen = self.tile_choice_distributions.sample()
                 
         
-        action = (self.coordinates_chosen,self.tile_chosen)
+        action = torch.column_stack((self.coordinates_chosen,self.tile_chosen.nonzero()[:,1]))
         
         return action
             
 
-    def give_reward(self, reward):
-        self.reward = reward
+    def give_reward(self, rewards):
+        self.rewards = torch.tensor(rewards)
 

kingdomino.py
@@ -16,8 +16,6 @@ class GameException(Exception):
     def __init__(self, msg):
         self.msg = msg
 
-
-
 class TileDeck:      
     def __init__(self):
         self.tile_data = GET_TILE_DATA() # array of tiles
@@ -65,17 +63,18 @@ class Kingdomino(gym.Env):
             self.n_players = n_players
         self.observation_space = spaces.Dict(
             {
-                'Boards': spaces.Box(low=-2, high=N_TILE_TYPES-1, shape=(self.n_players, 2, 9, 9), dtype=int),
-                'Previous tiles': spaces.Box(low=-2, high=N_TILE_TYPES-1, shape=(self.n_players, 5), dtype=int),
-                'Current tiles': spaces.Box(low=-2, high=N_TILE_TYPES-1, shape=(self.n_players, 6), dtype=int)
+                'Boards': spaces.Box(low=-2, high=N_TILE_TYPES-1, shape=(self.n_players, 2, 9, 9), dtype=float),
+                'Previous tiles': spaces.Box(low=0, high=100, shape=(self.n_players, TILE_SIZE), dtype=float),
+                'Current tiles': spaces.Box(low=0, high=100, shape=(self.n_players, TILE_SIZE+1), dtype=float)
             }
         )
-        self.action_space = spaces.Dict(
-            {
-                'Position': spaces.Box(low=-9, high=9, shape=(2,), dtype=int),
-                'Selected tile': spaces.Box(low=0, high=self.n_players-1, shape=(1,), dtype=int) 
-            }
-        )
+        # self.action_space = spaces.Dict(
+        #     {
+        #         'Position': spaces.Box(low=0, high=9, shape=(2,), dtype=int),
+        #         'Selected tile': spaces.Box(low=0, high=self.n_players-1, shape=(1,), dtype=int) 
+        #     }
+        # )
+        self.action_space = spaces.Box(low=0, high=9, shape=(3,), dtype=float)
         self.render_mode = render_mode
         self.window = None
         self.clock = None
@@ -96,7 +95,7 @@ class Kingdomino(gym.Env):
             self.boards.append(Board())
         self.current_player_itr = 0
         self.startTurn()
-        return self._get_obs()
+        return self._get_obs(), {}
     
     
     @property
@@ -142,58 +141,70 @@ class Kingdomino(gym.Env):
     # tile_id           : id of tile he wants to pick if there is still tiles to pick
     # position_inversed : tuple (TilePosition, bool) if there is a tile the player has
     def step(self, action):
-        Printer.print(self.current_player_id, "'s turn")
-        Printer.print('Action :', action)
+        try:
+            Printer.print(self.current_player_id, "'s turn")
+            Printer.print('Action :', action)
 
-        tile_id, position = action
-        
-        # Select tile
-        if not self.last_turn:
-            self.pickTile(tile_id)
-
-        if not self.first_turn:
-            if not (position == Kingdomino.discard_tile).all():
-                self.placeTile(position, self.previous_tiles[self.current_player_id])
-                Printer.print(self.boards[self.current_player_id])
-            else:
-                Printer.print('Tile discarded')
-        self.current_player_itr += 1
+            position = action[:2].astype(int)
+            tile_id = action[-1].astype(int)
             
-        done = self.last_turn and \
-            self.current_player_itr == self.n_players-1
-        
-        state = self._get_obs()
-        
-        return state, 0, done, {}
+            truncated = False
+            try:
+                # Select tile
+                if not self.last_turn:
+                    self.pickTile(tile_id)
+                print('1')
+
+                if not self.first_turn:
+                    if not (position == Kingdomino.discard_tile).all():
+                        self.placeTile(position, self.previous_tiles[self.current_player_id])
+                        Printer.print(self.boards[self.current_player_id])
+                    else:
+                        Printer.print('Tile discarded')
+                self.current_player_itr += 1
+                    
+                terminated = self.last_turn and \
+                    self.current_player_itr == self.n_players-1
+                
+                state = self._get_obs()
+            except GameException as e:
+                Printer.print(e.msg)
+                truncated = True
+        except Exception as e:
+            print(e.msg)
+            
+        return state, 0, terminated, truncated, {}
         
     # Board : 9*9 tile type + 9*9 crowns
     # Current/Previous tiles : 2 tile type, 2 crowns, 1 which player has chosen it, 1 value of tile
     # Previous tiles : 2 tile type, 2 crowns
-    # TODO : ORDER PLAYERS NICELY !!!! (player asking first)
+    # TODO : ORDER PLAYERS NICELY !!!! (player playing first)
     def _get_obs(self):
         obs = {'Boards'         : np.zeros([self.n_players,2,9,9]),
                'Current tiles'  : np.zeros([self.n_players,TILE_SIZE+1]),
-               'Previous tiles' : np.zeros([self.n_players,TILE_SIZE+1])}
+               'Previous tiles' : np.zeros([self.n_players,TILE_SIZE])}
         for i in self.order:     
             obs['Boards'][i,0] = self.boards[i].board
             obs['Boards'][i,1] = self.boards[i].crown
-        # for i,player in enumerate(self.players):
-        #     obs['Boards'][i,0] = player.board.board
-        #     obs['Boards'][i,1] = player.board.crown
+
         obs['Boards'][[0,self.current_player_id]] = obs['Boards'][[self.current_player_id,0]]
         obs['Previous tiles'] = self.previous_tiles
-        obs['Previous tiles'][[0,self.current_player_id]] = obs['Previous tiles'][[self.current_player_id,0]]
-
         obs['Current tiles'][:,:-1] = self.current_tiles
-        obs['Current tiles'][:,-1] = self.current_tiles_player
+        obs['Current tiles'][:,-1] = (self.current_tiles_player == -1).astype(float)
+
+        # same order as boards
+        obs['Previous tiles'] = obs['Previous tiles'][self.order]
+        obs['Current tiles'] = obs['Current tiles'][self.order]
+
+        # player as first
+        obs['Previous tiles'][[0,self.current_player_id]] = obs['Previous tiles'][[self.current_player_id,0]]
         
         return obs
         
-
+    # error in there
     def pickTile(self, tile_id):
         if self.current_tiles_player[tile_id] != -1:
             raise GameException(f'Error : Player {self.current_player_id} cannot choose tile {tile_id} because it is already selected !')
-        
         self.new_order[tile_id] = self.current_player_id
         self.current_tiles_player[tile_id] = self.current_player_id
     

learning_test.py
@@ -2,30 +2,69 @@ import numpy as np
 import gym
 
 from kingdomino import Kingdomino
-from agent import RandomPlayer, PlayerAC
+from agent import RandomPlayer, PlayerAC, NetworksInfo
 from printer import Printer
 
 BATCH_SIZE = 3
 N_PLAYERS = 2
 
-envs = gym.vector.AsyncVectorEnv([
-    lambda: gym.make("KingDomino-v0", n_players=N_PLAYERS),
-    lambda: gym.make("KingDomino-v0", n_players=N_PLAYERS),
-    lambda: gym.make("KingDomino-v0", n_players=N_PLAYERS),
-])
+network_info = NetworksInfo(
+    in_channels=50, 
+    output_size=50, 
+    conv_channels=50, 
+    conv_l=2,
+    conv_kernel_size=5,
+    conv_stride=1,
+    pool_place=[0,1,0,1,0,1,0,1,0,1],
+    pool_kernel_size=2,
+    pool_stride=1,
+    board_rep_size=220,
+    board_fc_n=220,
+    board_fc_l=2, 
+    player_rep_size=210,
+    board_prev_tile_fc_l=2,
+    shared_rep_size=240,
+    shared_l=10, 
+    shared_n=210,
+    actor_l=2,
+    actor_n=200,
+    critic_l=2,
+    critic_n=240)
 
-agent = PlayerAC(
-    n_players = N_PLAYERS,
-    gamma = 0.99,
-    lr_a = 1e-4,
-    lr_c = 5e-4,
-    coordinate_std = 2
-)
-
-Printer.activated = True
-done = False
-state = envs.reset()
-for i in range(500):
-    action = agent.action(state)
-    states,rewards,dones,_ = envs.step(action)
-    agent.give_reward(rewards)
+if __name__ == '__main__':
+    agent = PlayerAC(
+        batch_size = BATCH_SIZE,
+        n_players = N_PLAYERS,
+        gamma = 0.99,
+        lr_a = 1e-4,
+        lr_c = 5e-4,
+        coordinate_std = 2,
+        network_info = network_info
+    )
+    
+    # env = gym.make('KingDomino-v0', n_players=2)
+    # Printer.activated = True
+    # done = False
+    # state,_ = env.reset()
+    # for e in range(100):
+    #     while True:
+    #         action = agent.action(state)
+    #         print(action)
+    #         state,reward,terminated,truncated,info = env.step(np.array(action))
+    #         agent.give_reward([reward])
+    #         if terminated or truncated:
+    #             break
+        
+    envs = gym.vector.AsyncVectorEnv([
+        lambda: gym.make("KingDomino-v0", n_players=N_PLAYERS) for i in range(BATCH_SIZE)
+    ])
+    
+    Printer.activated = False
+    done = False
+    states,_ = envs.reset()
+    for i in range(500):
+        print('Step', i)
+        actions = agent.action(states)
+        print(actions)
+        states,rewards,terminateds,truncateds,info = envs.step(np.array(actions))
+        agent.give_reward(rewards)

networks.py
@@ -25,64 +25,79 @@ class NeuralNetwork(nn.Module):
         for layer in self.layers:
             x = layer(x)
         return x
+    
+class CNN(nn.Module):
+    def __init__(self,
+                 in_channels,
+                 conv_channels, conv_kernel_size, conv_stride, l,
+                 pool_place, pool_kernel_size, pool_stride
+                 ):
+        super(CNN, self).__init__()
+        
+        layers = [nn.Conv2d(
+            in_channels = in_channels,
+            out_channels = conv_channels,
+            kernel_size = conv_kernel_size,
+            stride = conv_stride)
+            ]
+        for i in range(l):
+            layers.append(nn.ReLU())
+            if pool_place[i] != 0:
+                layers.append(nn.MaxPool2d(
+                    kernel_size = pool_kernel_size,
+                    stride = pool_stride))
+        self.layers = nn.ModuleList(layers)
+        
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
 
 class BoardNetwork(nn.Module):
-    def __init__(self, n_inputs, n_outputs):
+    def __init__(self, n_inputs, network_info):
         super(BoardNetwork, self).__init__()
         
-        self.conv1 = nn.Conv2d(in_channels=n_inputs, out_channels=20,
-                               kernel_size=(5,5)) # 1620
-        self.relu1 = nn.ReLU()
-        self.maxpool1 = nn.MaxPool2d(kernel_size=(2,2), stride=(1,1)) # 405
-
-        self.conv2 = nn.Conv2d(in_channels=20, out_channels=50,
-                               kernel_size=(5,5))
-        self.relu2 = nn.ReLU()
-        self.maxpool2 = nn.MaxPool2d(kernel_size=(2,2), stride=(1,1))
-
-        self.fc1 = nn.Linear(in_features=450, out_features=500) # not 100    
-        self.relu3 = nn.ReLU()
+        self.cnn = CNN(
+            in_channels = n_inputs,
+            conv_channels = network_info.conv_channels,
+            conv_kernel_size = network_info.conv_kernel_size,
+            conv_stride = network_info.conv_stride,
+            l = network_info.conv_l,
+            pool_place = network_info.pool_place,
+            pool_kernel_size = network_info.pool_kernel_size,
+            pool_stride = network_info.pool_stride
+            )
+        self.fc = NeuralNetwork(
+            input_size = 3200, # modify according to error...or compute conv accordingly
+            output_size = network_info.board_rep_size,
+            n = network_info.board_fc_n,
+            l = network_info.board_fc_l)
         
-        self.fc2 = nn.Linear(in_features=500, out_features=n_outputs)    
-        self.relu4 = nn.ReLU()
         
     # x : [batch_size, 2, 9, 9]
     def forward(self, x):
         x = F.pad(x, (2,2,2,2), 'constant', -1) # pad each dimensions
-        
-        x = self.conv1(x)
-        x = self.relu1(x)
-        x = self.maxpool1(x)
-        
-        x = self.conv2(x)
-        x = self.relu2(x)
-        x = self.maxpool2(x)
-        
+        x = self.cnn(x)
         x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = self.relu3(x)
-        
-        x = self.fc2(x)
-        x = self.relu4(x)
-        
+        x = self.fc(x)
         return x
 
 # /        Board network        /
 # Player board -> convo -> fc ->  fc -> Player specific vector
 #               Previous tile ->
 class PlayerNetwork(nn.Module):
-    def __init__(self, board_rep_size, player_rep_size):
+    def __init__(self, network_info):
         super(PlayerNetwork, self).__init__()
         
         self.board_network = BoardNetwork(
             n_inputs=N_TILE_TYPES+1,
-            n_outputs=board_rep_size)
+            network_info=network_info)
         
         self.join_info_network = NeuralNetwork(
-            input_size=board_rep_size + 2*TILE_ENCODING_SIZE,
-            output_size=player_rep_size, 
-            l=4,
-            n=player_rep_size)
+            input_size = network_info.board_rep_size + TILE_ENCODING_SIZE,
+            output_size = network_info.player_rep_size, 
+            l = network_info.board_prev_tile_fc_l,
+            n = network_info.player_rep_size)
 
     def forward(self, board, previous_tile):
         board_rep = self.board_network(board)
@@ -92,35 +107,31 @@ class PlayerNetwork(nn.Module):
         
 
 class Shared(nn.Module):
-    def __init__(self, batch_size, n_players, board_rep_size, player_rep_size, shared_rep_size):
+    def __init__(self, n_players, network_info):
         super(Shared, self).__init__()
-        self.batch_size = batch_size
         self.n_players = n_players
-        self.board_rep_size = board_rep_size
-        self.player_rep_size = player_rep_size
-        self.shared_rep_size = shared_rep_size
+        self.network_info = network_info
         
-        self.player_network = PlayerNetwork(
-            board_rep_size=self.board_rep_size,
-            player_rep_size=self.player_rep_size)
+        self.player_network = PlayerNetwork(network_info)
         
-        self.shared_input_size = self.player_rep_size * self.n_players \
-            + TILE_ENCODING_SIZE
-        
+        shared_input_size = \
+            (self.network_info.player_rep_size + TILE_ENCODING_SIZE+1) * self.n_players
+            
         self.shared_network = NeuralNetwork(
-            input_size=self.shared_input_size,
-            output_size=self.shared_rep_size,
-            l=4, 
-            n=200)
+            input_size=shared_input_size,
+            output_size=self.network_info.shared_rep_size,
+            l=self.network_info.shared_l, 
+            n=self.network_info.shared_n)
 
     # x['Boards'] : (batch_size, n_players, 2, 9, 9)
     # Returns : (batch_size, n_players, N_TILE_TYPES+2, 9, 9)
     def boards2onehot(self, x):
+        batch_size = x['Boards'].size()[0]
         board_size = x['Boards'].size()[-1]
         ## BOARDS
         # (N_TILE_TYPES) + 2 : crown + empty tiles
         boards_one_hot = torch.zeros([
-            self.batch_size,
+            batch_size,
             self.n_players,
             N_TILE_TYPES+2,
             board_size,board_size])
@@ -131,9 +142,9 @@ class Shared(nn.Module):
 
     # x['Previous tiles'] : (batch_size, n_players, TILE_SIZE (5))
     def tile2onehot(self, tiles):
-        ## PREV TILES
+        batch_size = tiles.size()[0]
         prev_tiles_info = torch.zeros([
-            self.batch_size,
+            batch_size,
             self.n_players,
             TILE_ENCODING_SIZE])
         prev_tiles_info[:,:,-1] = tiles[:,:,-1] # Value
@@ -146,8 +157,9 @@ class Shared(nn.Module):
     # Assumes the observation at 0 is current player
     # Additional information : taken or not, and by whom
     def current_tiles_vector(self, x):
+        batch_size = x['Boards'].size()[0]
         current_tiles_info = torch.zeros([
-            self.batch_size,
+            batch_size,
             self.n_players,
             TILE_ENCODING_SIZE+1])
         current_tiles_info[:,:,:-1] = self.tile2onehot(x['Current tiles'][:,:,:-1])
@@ -156,46 +168,32 @@ class Shared(nn.Module):
     
     # Assumes the observation at 0 is current player
     def players_vector(self, x):
-        boards_one_hot = self.boards2onehot(x)
-        previous_tile_one_hot = self.tile2onehot(x['Previous tiles'])
-        
+        with torch.no_grad():
+            batch_size = x['Boards'].size()[0]
+            boards_one_hot = self.boards2onehot(x)
+            previous_tile_one_hot = self.tile2onehot(x['Previous tiles'])
+            
         players_output = torch.zeros([
-            self.batch_size,
+            batch_size,
             self.n_players,
-            self.board_rep_size])
-        # TODO : parallelize this
-        for i in range(self.n_players):
-            players_output[:,i] = self.player_network(
-                board=boards_one_hot[:,i],
-                previous_tile=previous_tile_one_hot[:,i])
-        return players_output.reshape(self.batch_size, -1)
-    
+            self.network_info.player_rep_size])
+        # TODO : parallelize this ?
+        # for i in range(self.n_players):
+        #     players_output[:,i] = self.player_network(
+        #         board=boards_one_hot[:,i],
+        #         previous_tile=previous_tile_one_hot[:,i])
+        return players_output.reshape(batch_size, -1)
 
-    
-
-
-    
-    # def curr_tiles_vector(self, player_id, x):
-    #     curr_tiles_info = torch.zeros([self.n_players, 2*(N_TILE_TYPES+2+1)+self.n_players-1])
-    #     # + n_players : corresponds to one_hot_encoded player_id
-    #     curr_tiles_info[:,:N_TILE_TYPES] = F.one_hot(x['Current tiles'][:,0], num_classes=N_TILE_TYPES)
-    #     curr_tiles_info[:,N_TILE_TYPES:2*(N_TILE_TYPES+2)] = F.one_hot(x['Current tiles'][:,1], num_classes=N_TILE_TYPES)
-    #     curr_tiles_info[:,2*N_TILE_TYPES] = x['Current tiles'][:,2]
-    #     curr_tiles_info[:,2*N_TILE_TYPES+1] = x['Current tiles'][:,3]
-    #     curr_tiles_info[:,2*(N_TILE_TYPES+2)+1:2*(N_TILE_TYPES+2)+1+self.n_players-1] = \
-    #         F.one_hot(x['Current tiles'][:,5], num_classes=self.n_players-1) \
-    #             * (x['Current tiles'][:,5] != -1)
-    #     return curr_tiles_info.flatten()
-
-    # equivalence of players not taken into account (should share weights) 
-    def forward(self, player_id, x):
-        print(x)
-        for key in x:
-            x[key] = torch.tensor(x[key], dtype=torch.int64)
-        players_vector = self.players_vector(player_id, x).reshape(self.batch_size, -1)
-        current_tiles_vector = self.current_tiles_vector(player_id, x)
-        
-        x = torch.cat([players_vector, current_tiles_vector], dim=1)
+    # equivalence of players not taken into account (ideally should share weights) 
+    def forward(self, x):
+        with torch.no_grad():
+            for key in x:
+                x[key] = torch.tensor(x[key], dtype=torch.int64)
+            batch_size = x['Boards'].size()[0]
+        players_vector = self.players_vector(x).reshape(batch_size, -1)
+        with torch.no_grad():
+            current_tiles_vector = self.current_tiles_vector(x)
+        x = torch.cat([players_vector, current_tiles_vector.reshape(batch_size,-1)], dim=1)
         x = self.shared_network(x)
         return x
 